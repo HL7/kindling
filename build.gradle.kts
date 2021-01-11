85fdb3e6f12420a7f09d79bd33ab0fd87f5ace4b
markiantorno <markiantorno@gmail.com>
2021-01-11 14:32:44 -0500

    Updating gradle build file so it points to source.

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,3 +135,26 @@ dependencies {
 
     testImplementation("junit", "junit", "4.12")
 }
+
+configurations {
+    implementation {
+        resolutionStrategy.failOnVersionConflict()
+    }
+}
+
+sourceSets {
+    main {
+        java.srcDir("src/main/java")
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    test {
+        testLogging.showExceptions = true
+    }
+}
