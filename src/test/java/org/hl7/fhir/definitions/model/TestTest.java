b90114261c2625df68d59662d969c84e58777787
dotasek <david.otasek@smilecdr.com>
2022-09-26 10:56:05 -0400

    Test a test

---
+++ src/test/java/org/hl7/fhir/definitions/model/TestTest.java
@@ -0,0 +1,16 @@
+package org.hl7.fhir.definitions.model;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void testTheTests() {
+        System.out.println("Testing, 1, 2, 3.");
+        assertTrue(true, "Should be true.");
+    }
+
+}
