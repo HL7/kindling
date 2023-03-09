580e8c377d5da44306434ac9391d771f93bd43d9
Grahame Grieve <grahameg@gmail.ccom>
2023-03-09 19:51:17 +1100

    fix bugs testing search expressions

--- src/main/java/org/hl7/fhir/tools/publisher/ExampleInspector.java
+++ src/main/java/org/hl7/fhir/tools/publisher/ExampleInspector.java
@@ -82,7 +82,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 
-public class ExampleInspector implements IValidatorResourceFetcher, IValidationPolicyAdvisor {
+public class ExampleInspector implements IValidatorResourceFetcher, IValidationPolicyAdvisor, IEvaluationContext {
 
   public static class EValidationFailed extends Exception {
     private static final long serialVersionUID = 1538324138218778487L;
@@ -217,6 +217,7 @@ public class ExampleInspector implements IValidatorResourceFetcher, IValidationP
     validator.setForPublication(true);
     
     fpe = new FHIRPathEngine(context);
+    fpe.setHostServices(this);
   }
 
   public void prepare2() throws Exception {
@@ -738,6 +739,69 @@ public class ExampleInspector implements IValidatorResourceFetcher, IValidationP
       System.out.println("Didn't find invariant for "+f.getName());
       return false;
     }
+  }
+
+  @Override
+  public List<Base> resolveConstant(Object appContext, String name, boolean beforeContext) throws PathEngineException {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public TypeDetails resolveConstantType(Object appContext, String name) throws PathEngineException {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public boolean log(String argument, List<Base> focus) {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public FunctionDetails resolveFunction(String functionName) {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public TypeDetails checkFunction(Object appContext, String functionName, List<TypeDetails> parameters) throws PathEngineException {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public List<Base> executeFunction(Object appContext, List<Base> focus, String functionName, List<List<Base>> parameters) {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public Base resolveReference(Object appContext, String url, Base refContext) throws FHIRException {
+    if (Utilities.charCount(url, '/') == 1) {
+     String type = url.substring(0, url.indexOf("/"));
+     String id = url.substring(url.indexOf("/")+1);
+     ResourceDefn rd = definitions.getResourceByName(type);
+     if (rd != null) {
+       for (Example ex : rd.getExamples()) {
+         if (ex.getId().equals(id)) {
+           if (ex.getResource() != null) {
+             return ex.getResource();
+           } else {
+             return ex.getElement(); // new XmlParser().parse(ex.getXml());
+           }
+         }
+       }
+     }
+     return null;
+    } else {
+      throw new NotImplementedException();
+    }
+  }
+
+  @Override
+  public boolean conformsToProfile(Object appContext, Base item, String url) throws FHIRException {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public ValueSet resolveValueSet(Object appContext, String url) {
+    throw new NotImplementedException();
   }
 
 
