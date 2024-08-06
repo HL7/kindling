7441123b847f8f48eb8ccf33c3ed8c8eabb10f8f
Gino Canessa <gino.canessa@microsoft.com>
2024-08-06 09:36:43 -0500

    Changed xml mapping writer to use System.lineSeparator instead of \n.

--- src/main/java/org/hl7/fhir/tools/publisher/PageProcessor.java
+++ src/main/java/org/hl7/fhir/tools/publisher/PageProcessor.java
@@ -6802,7 +6802,7 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider, IReferen
 
           java.io.Writer writer = new java.io.FileWriter(exceptionsFile);
           final LSSerializer xmlWriter = impl.createLSSerializer();
-          xmlWriter.setNewLine("\n");
+          xmlWriter.setNewLine(System.lineSeparator());
           xmlWriter.getDomConfig().setParameter("format-pretty-print", true);
           xmlWriter.getDomConfig().setParameter("xml-declaration", false);
           String xml = xmlWriter.writeToString(newExceptionsDoc);
