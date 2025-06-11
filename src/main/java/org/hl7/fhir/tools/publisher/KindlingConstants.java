5430a7a0cf8c8a726e0c3c20f4c59ed3ebd70776
Richard Ettema <richard.ettema@aegis.net>
2025-06-11 16:26:48 -0400

    Issue #189 - Fix tx server url suffix

--- src/main/java/org/hl7/fhir/tools/publisher/KindlingConstants.java
+++ src/main/java/org/hl7/fhir/tools/publisher/KindlingConstants.java
@@ -3,7 +3,7 @@ package org.hl7.fhir.tools.publisher;
 import org.hl7.fhir.utilities.settings.FhirSettings;
 
 public class KindlingConstants {
-  
-  public final static String DEF_TS_SERVER = FhirSettings.getTxFhirProduction()+"/r4"; 
+
+  public final static String DEF_TS_SERVER = FhirSettings.getTxFhirProduction()+"/r5";
 
 }
