395a7b47b165b49a3f2f77d3cb23d0ee35f6f89e
Grahame Grieve <grahameg@gmail.ccom>
2025-07-12 08:20:52 +1000

    fix server address

--- src/main/java/org/hl7/fhir/tools/publisher/KindlingConstants.java
+++ src/main/java/org/hl7/fhir/tools/publisher/KindlingConstants.java
@@ -4,6 +4,6 @@ import org.hl7.fhir.utilities.settings.FhirSettings;
 
 public class KindlingConstants {
   
-  public final static String DEF_TS_SERVER = FhirSettings.getTxFhirProduction()+"/r4"; 
+  public final static String DEF_TS_SERVER = FhirSettings.getTxFhirProduction()+"/r5";
 
 }
